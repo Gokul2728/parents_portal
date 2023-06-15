244e51e03adc924a67cb2c468cadd298fdf3fcbf
Gokul_@bitbucket.org <in7083@bitsathy.ac.in>
2023-06-15 22:53:17 +0530

    committed

---
+++ lib/Screens/achievements.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Achievements extends StatefulWidget {
+  const Achievements({Key? key}) : super(key: key);
+
+  @override
+  _AchievementsState createState() => _AchievementsState();
+}
+
+class _AchievementsState extends State<Achievements> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('data'),
+    );
+  }
+}
---
+++ lib/Screens/home.dart
@@ -0,0 +1,299 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Column(
+          children: [
+            Container(
+              height: 20,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0XFF00B686), Color(0XFF00838F)]),
+              ),
+            ),
+            Container(
+              height: 300,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0XFF00B686), Color(0XFF00838F)]),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20, right: 20.0, top: 30),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const [
+                        Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 20),
+                        Text(
+                          "Dashboard",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: 80.0,
+                          height: 80.0,
+                          decoration: BoxDecoration(
+                            color: const Color(0XFF00B686),
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black.withOpacity(.1),
+                                  blurRadius: 8,
+                                  spreadRadius: 3)
+                            ],
+                            border: Border.all(
+                              width: 1.5,
+                              color: Colors.white,
+                            ),
+                            borderRadius: BorderRadius.circular(40.0),
+                          ),
+                          padding: const EdgeInsets.all(5),
+                          child: const CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://images.pexels.com/photos/2167673/pexels-photo-2167673.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Christopher Summers",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.camera_front,
+                                  color: Colors.white,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                RichText(
+                                  text: const TextSpan(
+                                      text: "\$5320",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                      children: [
+                                        TextSpan(
+                                            text: ".50",
+                                            style: TextStyle(
+                                                color: Colors.white38))
+                                      ]),
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                color: Colors.grey.shade100,
+                child: ListView(
+                  padding: const EdgeInsets.only(top: 75),
+                  children: [
+                    const Text(
+                      "Activity",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const Text(
+                      "Categories",
+                      style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+        Positioned(
+          top: 185,
+          right: 0,
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            width: MediaQuery.of(context).size.width * 0.85,
+            height: 160,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(.05),
+                    blurRadius: 8,
+                    spreadRadius: 3,
+                    offset: const Offset(0, 10),
+                  ),
+                ],
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  bottomLeft: Radius.circular(50),
+                )),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: const [
+                            Text(
+                              "Income",
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Icon(
+                              Icons.arrow_upward,
+                              color: Color(0XFF00838F),
+                            )
+                          ],
+                        ),
+                        const Text(
+                          "\$2 170.90",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                              color: Colors.black87),
+                        )
+                      ],
+                    ),
+                    Container(width: 1, height: 50, color: Colors.grey),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: const [
+                            Text(
+                              "Expenses",
+                              style: TextStyle(
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Icon(
+                              Icons.arrow_downward,
+                              color: Color(0XFF00838F),
+                            )
+                          ],
+                        ),
+                        const Text(
+                          "\$1 450.10",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                              color: Colors.black87),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "You spent \$ 1,494 this month",
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                const Text(
+                  "Let's see the cost statistics for this period",
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 1,
+                  width: double.maxFinite,
+                  color: Colors.grey.withOpacity(0.5),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: const Text(
+                    "Tell me more",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Color(0XFF00B686)),
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
